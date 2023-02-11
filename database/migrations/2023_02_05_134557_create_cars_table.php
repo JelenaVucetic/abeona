cadc5acf786f987236eb6096368a350f82b703b1
d-vucetic <d.vucetic@vegait.rs>
2023-02-11 13:18:14 +0100

    auth, relacije, storage start

--- database/migrations/2023_02_05_134557_create_cars_table.php
+++ database/migrations/2023_02_05_134557_create_cars_table.php
@@ -15,6 +15,12 @@ return new class extends Migration
     {
         Schema::create('cars', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->integer('year');
+            $table->string('motor');
+            $table->integer('doors');
+            $table->integer('passanger');
+            $table->string('transmission');
             $table->timestamps();
         });
     }
